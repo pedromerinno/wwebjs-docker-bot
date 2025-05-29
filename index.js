7010245e525352cf731e9dced5cb9e458b4510c1
pedromerinno <118215765+pedromerinno@users.noreply.github.com>
2025-05-29 12:26:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.post('/send-report', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Servidor HTTP escutando na porta ${PORT}`);
 });
 
